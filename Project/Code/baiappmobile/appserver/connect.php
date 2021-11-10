d700785d2923c0ab661b2a52f8597dd993b3cd4c
trandduchuy <91442648+trandduchuy@users.noreply.github.com>
2021-11-10 19:54:42 +0700

    huy
    commit huy

---
+++ Project/Code/baiappmobile/appserver/connect.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$database = "thietbidb";
+
+$conn = mysqli_connect($host,$user,$pass,$database);
+mysqli_set_charset($conn,"utf8");
+
+
+
+
+?>
